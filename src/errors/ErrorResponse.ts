f7f6b37555df195530b834f58a914f585df4334f
MPesin <mikelpesin@gmail.com>
2024-02-04 21:06:25 +0200

    rename desc. to message

--- src/errors/ErrorResponse.ts
+++ src/errors/ErrorResponse.ts
@@ -3,17 +3,17 @@ import createError, {HttpError} from 'http-errors';
 export default class ErrorResponse extends Error {
   private readonly _error: HttpError;
 
-  constructor(statusCode: number, description: string = "") {
+  constructor(statusCode: number, message: string = "") {
     super();
-    this._error = description ? createError(statusCode, description) : createError(statusCode);
+    this._error = message ? createError(statusCode, message) : createError(statusCode);
   }
 
   get statusCode(): number {
     return this._error.statusCode;
   }
 
-  get description(): string {
-    return this._error.description;
+  get message(): string {
+    return this._error.message;
   }
 
   get httpError(): HttpError {
