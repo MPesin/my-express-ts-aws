650c5378d4047495ee121ac1bd87c304fb00563f
MPesin <mikelpesin@gmail.com>
2024-02-04 21:52:40 +0200

    rename

--- src/infrastructure/envVars.ts
+++ src/infrastructure/envVars.ts
@@ -1,6 +1,6 @@
 
 const envVars = {
-  nodeEnvironment: process.env.NODE_ENV ?? "development",
+  environment: process.env.NODE_ENV ?? "development",
   port: process.env.PORT ?? "8000",
   jwtSecretName: process.env.JWT_SECRET_NAME ?? "",
   dbUsername: process.env.DB_USERNAME ?? "",
